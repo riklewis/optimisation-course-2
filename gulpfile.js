131827e59cc9f53db82e8761d972d40ac191fe51
Rik Lewis <rik@rik.onl>
2019-06-03 22:14:05 +0100

    Ready for Leverage Browser Caching

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,35 @@
 var gulp = require("gulp");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var optimize = require("gulp-optimize-js");
-var maps = require("gulp-sourcemaps");
-var cleancss = require("gulp-clean-css");
-var imagemin = require("gulp-imagemin");
-var newer = require("gulp-newer");
-var htmlmin = require("gulp-htmlmin");
+var plugins = require("gulp-load-plugins")();
 
 var root = function() {
   return gulp.src("index.htm")
-    .pipe(htmlmin({collapseWhitespace:true,removeComments:true}))
+    .pipe(plugins.htmlmin({collapseWhitespace:true,removeComments:true}))
     .pipe(gulp.dest("build"));
 };
 
 var js = function() {
   return gulp.src(["js/optimisation.js","js/debug.js"])
-    .pipe(maps.init())
-    .pipe(concat("script.js"))
-    .pipe(uglify())
-    .pipe(optimize())
-    .pipe(maps.write("../maps"))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat("script.js"))
+    .pipe(plugins.uglify())
+    .pipe(plugins.optimizeJs())
+    .pipe(plugins.sourcemaps.write("../maps"))
     .pipe(gulp.dest("build/js"));
 };
 
 var css = function() {
   return gulp.src(["css/*.css"])
-    .pipe(maps.init())
-    .pipe(concat("style.css"))
-    .pipe(cleancss({level:{1:{specialComments:0},2:{removeDuplicateRules:true}}}))
-    .pipe(maps.write("../maps"))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat("style.css"))
+    .pipe(plugins.cleanCss({level:{1:{specialComments:0},2:{removeDuplicateRules:true}}}))
+    .pipe(plugins.sourcemaps.write("../maps"))
     .pipe(gulp.dest("build/css"));
 };
 
 var img = function() {
   return gulp.src(["img/*.png","img/*.jpg","img/*.gif","img/*.svg"])
-    .pipe(newer("build/img"))
-    .pipe(imagemin())
+    .pipe(plugins.newer("build/img"))
+    .pipe(plugins.imagemin())
     .pipe(gulp.dest("build/img"));
 };
 
